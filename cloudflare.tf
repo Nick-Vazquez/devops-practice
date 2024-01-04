16d8f8e74c29f2c6df0eca69644401077a06b751
Nick Vazquez <nmv@iastate.edu>
2024-01-04 16:15:00 -0600

    Cycle exposed CF API key

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,29 +1,35 @@
 ### Cloudflare ###
 
+variable "cloudflare_api_token" {
+    type = string
+    description = "Cloudflare API Token"
+    sensitive = true
+}
+
 provider "cloudflare" {
-  api_token = "o0fZ1c5K0UecuBVZm2cO1vFsylg41JWNht5i_Lth"
+  api_token = var.cloudflare_api_token
 }
 
-variable "zone_id" {
+variable "cloudflare_zone_id" {
   type        = string
-  default     = "e5333a972eacce7e771ea4532eb5d7c8"
   description = "Cloudflare Zone ID"
+  sensitive   = true
 }
 
-variable "account_id" {
+variable "cloudflare_account_id" {
     type        = string
-    default     = "534018189da378fd664bcc271c90f668"
     description = "Cloudflare Account ID"
+    sensitive   = true
 }
 
-variable "domain" {
+variable "cloudflare_domain" {
   type        = string
   default     = "nickvazquez.com"
   description = "Cloudflare Domain name"
 }
 
 resource "cloudflare_record" "www" {
-  zone_id = var.zone_id
+  zone_id = var.cloudflare_zone_id
   name    = "test1"
   value   = aws_instance.app_server.public_ip
   type    = "A"
